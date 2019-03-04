0aee2616762cb1f5e0bf28d138d306ff0a0db938
Santosh <santosh.v@idsnext.com>
2019-03-04 10:28:09 +0530

    Code modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  result:string = `<div>Proin efficitur tellus blandit, dictum ante ut, posuere ligula. Duis suscipit ante efficitur risus tincidunt cursus.</div> 
-  <div><br></div><div>Aliquam in dui vel erat euismod volutpat. Morbi pulvinar, tortor at aliquam viverra, odio neque molestie urna, sed tempus nibh ligula vel ligula. Sed aliquet mi a tortor rutrum fermentum. Suspendisse ut velit diam. Maecenas ligula enim, rhoncus mattis lorem ut, tincidunt varius tortor.&nbsp;</div><div><br></div><div>Cras ex nunc, vestibulum ut augue et, tincidunt fringilla dolor. Nam laoreet turpis in arcu commodo ultrices. Cras eget sapien ac velit accumsan tempor ut maximus mauris. Nunc sem odio, pellentesque sit amet magna sed, gravida consectetur massa. Etiam sodales convallis purus eget condimentum. Mauris ultricies purus vel ultricies tempus.</div>`;
-
-  constructor(){
+  result: string = `<div>555 jj.</div>`;
+  options:any = {
+    "background-color": "#415b8a",
+    "color": "#ffffff",
+    "border": "2px solid #415b8a"
+  }
+  constructor() {
     // this.editorContent = "eee"
   }
 
-  newData(elem){
+  update(elem) {
     console.log(elem);
-this.result = elem;
+    this.result = elem;
+  }
+  view:boolean = false;
+  show(){
+    this.view = true;
   }
 }
--- src/app/skv-editor/skv-editor.component.ts
+++ src/app/skv-editor/skv-editor.component.ts
@@ -1,132 +1,113 @@
-import { Component, OnInit,ViewChild, ElementRef,Input,forwardRef,EventEmitter,Output } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef, Input, EventEmitter, Output, Renderer2 } from '@angular/core';
 
 
 
 @Component({
   selector: 'skv-editor',
   templateUrl: './skv-editor.component.html',
-  styleUrls: ['./skv-editor.component.css'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => SkvEditorComponent),
-      multi: true
-    }
-  ]
+  styleUrls: ['./skv-editor.component.css']
 })
-export class SkvEditorComponent implements ControlValueAccessor {
-  @Input('data') data:string;
+export class SkvEditorComponent implements OnInit {
+  @Input('data') data: string;
+  @Input('options') options?: any;
+
   @ViewChild('iframe') iframe: ElementRef;
-  @Output() update = new EventEmitter;
-  // value:string = ''
-  private _onChange = (_: any) => {};
-  private _onTouched = () => {};
-   iframeDocRef:any;
-  fontList:any[]=[];
-  fontSizeList:string[]=[];
-  isViewMode:boolean = false;
-  isSourceVisible:boolean = false;
-  constructor() {
+  @Output() onUpdate = new EventEmitter;
+  globalListenFunc: Function;
+
+  iframeDocRef: any;
+  fontList: any[] = [];
+  fontSizeList: string[] = [];
+  isViewMode: boolean = false;
+  isSourceVisible: boolean = false;
+
+  constructor(private renderer: Renderer2) {
     this.fontList = [
-      {"key":"Times New Roman","name":"Times New Roman"},
-      {"key":"consolas","name":"Consolas"},
-      {"key":"tahoma","name":"Tahoma"},
-      {"key":"monospace","name":"Monospace"},
-      {"key":"cursive","name":"Cursive"},
-      {"key":"sans-serif","name":"Sans-serif"},
-      {"key":"calibri","name":"Calibri"},
+      { "key": "Times New Roman", "name": "Times New Roman" },
+      { "key": "consolas", "name": "Consolas" },
+      { "key": "tahoma", "name": "Tahoma" },
+      { "key": "monospace", "name": "Monospace" },
+      { "key": "cursive", "name": "Cursive" },
+      { "key": "sans-serif", "name": "Sans-serif" },
+      { "key": "calibri", "name": "Calibri" },
     ]
-    for(let i=1; i<=7; i++){
+    for (let i = 1; i <= 7; i++) {
       this.fontSizeList.push(i + "");
     }
-   }
-  ngAfterViewInit() {
-    // let content:string = ``;
-    // doc.open();
-    // doc.write(content);
-    // doc.close();
-    // console.log(this.data)
-    this.iframeDocRef =  this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentWindow;
+  }
+  ngOnInit() {
+
+    console.log(this.options);
+    if (!this.options) {
+      this.options = {
+        "background-color": "#fd903f",
+        "color": "#ffffff",
+        "border": "2px solid #fd903f"
+      }
+    }
+    this.iframeDocRef = this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentWindow;
     this.iframeDocRef.designMode = "on";
     this.iframeDocRef.open();
     this.iframeDocRef.write(this.data);
     this.iframeDocRef.close();
 
-    this.iframeDocRef.body.addEventListener('keyup',(e)=>{
+    this.globalListenFunc = this.renderer.listen(this.iframeDocRef.body, 'input', e => {
       this.newEmit(e.target.innerHTML);
-      // this._onChange(this.iframeDocRef.body.textContent);
-      // this._onTouched(this.iframeDocRef.body.textContent);
-    },true)
+    });
   }
- 
 
-  executeCommand(cmd:any){
-    this.iframeDocRef.execCommand(cmd,false,null);
+  executeCommand(cmd: any) {
+    this.iframeDocRef.execCommand(cmd, false, null);
   }
-  executeCommandWithOption(cmd1:string,cmd2:boolean,cmd3:any){
-    let newCmd:any = cmd3;
-    switch(cmd1){
-      case 'insertorderedlist': newCmd = 'newOL' + Math.round(Math.random()*1000); break;
+  executeCommandWithOption(cmd1: string, cmd2: boolean, cmd3: any) {
+    let newCmd: any = cmd3;
+    switch (cmd1) {
+      case 'insertorderedlist': newCmd = 'newOL' + Math.round(Math.random() * 1000); break;
       case 'createlink': newCmd = prompt("Enter URL "); break;
     }
-    this.iframeDocRef.execCommand(cmd1,cmd2,newCmd);
-    
+    this.iframeDocRef.execCommand(cmd1, cmd2, newCmd);
+
   }
-  viewEditMode(){
+  viewEditMode() {
     console.log(this.isViewMode);
-    if(this.isViewMode){
-    this.iframeDocRef.designMode = "on";
-    this.isViewMode = false;
-    
+    if (this.isViewMode) {
+      this.iframeDocRef.designMode = "on";
+      this.isViewMode = false;
+
     }
-    else{
+    else {
       this.iframeDocRef.designMode = "off";
-    this.isViewMode = true;
-    
+      this.isViewMode = true;
+
     }
   }
-  sourceCode(){    
-    if(this.isSourceVisible){
-      this.iframeDocRef.body.innerHTML = this.iframeDocRef.body.textContent;      
+  sourceCode() {
+    if (this.isSourceVisible) {
+      this.iframeDocRef.body.innerHTML = this.iframeDocRef.body.textContent;
       this.isSourceVisible = false;
-    }else{
-      this.iframeDocRef.body.textContent= this.iframeDocRef.body.innerHTML;      
+      this.iframeDocRef.designMode = "on";
+      
+    } else {
+      this.iframeDocRef.body.textContent = this.iframeDocRef.body.innerHTML;
       this.isSourceVisible = true;      
+      this.iframeDocRef.designMode = "off";
     }
-    
+
   }
-  insertImage(elem:any){
+  insertImage(elem: any) {
+    if (this.isSourceVisible) return;
     if (elem.files && elem.files[0]) {
       var reader = new FileReader();
-      reader.onload =  (e:any) => {
-       console.log(e.target.result);
-       this.iframeDocRef.execCommand('insertImage',false,e.target.result);
+      reader.onload = (e: any) => {
+        console.log(e.target.result);
+        this.iframeDocRef.execCommand('insertImage', false, e.target.result);
       };
       reader.readAsDataURL(elem.files[0]);
     }
   }
-  newEmit(content:any){
-    this.writeValue(content);
-    this.registerOnChange(content);
-    this.update.emit(content);
-    
-  }
-  writeValue(obj: any): void {
-    this.data = obj;
-    // throw new Error("Method not implemented.");
+  newEmit(content: any) {
+    this.onUpdate.emit(content);
+
   }
 
-
-  setDisabledState?(isDisabled: boolean): void {
-    // throw new Error("Method not implemented.");
-  }
- 
-  registerOnChange( fn : any ) : void {
-    this._onChange = fn;
-  }
-
-  registerOnTouched( fn : any ) : void {
-    this._onTouched = fn;
-  } 
 }
